d2675dd5faedeffe615064881ea38b7974c740bd
Julian Norton <julian@juliannorton.com>
2018-03-07 16:28:56 -0500

    implementing basic fetch and response

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # python_noaa_weather
 
-Set-up
+## Set-up
 
 1. Get API key from the NCDC https://www.ncdc.noaa.gov/cdo-web/token
 2. `pip install flask`
-3. Create a new file named `secret.py` and put your api key inside:
+3. Create a new file named `api_keys.py` and put your api key inside:
 	```
-		def api_key():
-			return '<keyhere>'
+	noaa_api_key = '<api key>'
 	```
+
+
+### NOAA weather API documentation
+* https://www.ncdc.noaa.gov/cdo-web/webservices/v2
--- request_weather.py
+++ request_weather.py
@@ -1,24 +1,32 @@
 from flask import Flask
-app = Flask(__name__)
-
-# API key is stored here, not committed
-import secret
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
-
-
+import api_keys # TODO, handle enviromental secrets better. API key is stored here, not committed
 # To CURL a URL
 import requests
 
-noaa_api_key = secret.api_key()
+app = Flask(__name__)
+noaa_api_key = noaa_api_key.api_keys
+# TODO learn about flask config
 
-print(noaa_api_key)
+@app.route('/')
+def index():
+    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CITY&sortfield=name&sortorder=desc'
+    headers = {'token': secret.api_key}
+    r = requests.get(url, headers=headers)
+    # python string format function
+    
+    return '''<html>
+    <head>
+        <title>Home Page - Test</title>
+    </head>
+    <body>
+        <h1>{weather}</h1>
+        <h2>{lightning}
+    </body>
+</html>'''.format(weather=str(r.json()),lightning='tacos')
+
+# This has to be at bottom to run
+if __name__ == '__main__':
+    app.run()
 
 # https://realpython.com/blog/python/api-integration-in-python/
 
