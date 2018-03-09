989891aa3fcd8d2d0e00a32023870c35ee74771e
Julian Norton <Julian@JulianNorton.com>
2018-03-09 14:37:33 -0500

    rendering with sample json instead of api response

--- request_weather.py
+++ request_weather.py
@@ -1,53 +1,41 @@
 from flask import Flask
-import api_keys # TODO, handle enviromental secrets better. API key is stored here, not committed
-# To CURL a URL
-import requests
+import requests # to curl a URL
+import json # process sample json
 
-
-# Just webscrape instead?
-# todo, https://forecast.weather.gov/MapClick.php?lat=40.7198&lon=-73.993&FcstType=digitalDWML
-# https://forecast.weather.gov/MapClick.php?lat=40.7979&lon=-73.9664&unit=0&lg=english&FcstType=text&TextType=1
-
-app = Flask(__name__)
-noaa_api_key = ""
 # TODO learn about flask config
 
-url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories?stationid=GHCND:USW00094728"
-url = url + "&" "startdate=2018-01-01"
-url = url + "&" "enddate=2018-01-02"
+
+# Import sample json data, #TODO, load dynamically and cache
+with open('sample-forecast.json') as json_data:
+    sample_forecast = json.load(json_data)
+
+
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CITY&sortfield=name&sortorder=desc'
-    headers = {'token': noaa_api_key}
-    r = requests.get(url, headers=headers)
-    # python string format function
-    
+def index():    
     return '''<html>
     <head>
         <title>Home Page - Test</title>
     </head>
     <body>
-        <p>{request}</p>
         <p>{weather}</p>
-        <h2>{lightning}</h2>
+        <p>{test}</p>
     </body>
-</html>'''.format(request=str(url), weather=str(r.json()),lightning='\n\n—')
+</html>'''.format(weather=str(sample_forecast),test='hello')
+
 
 # This has to be at bottom to run
 if __name__ == '__main__':
     app.run()
 
 
-"""
-    {'metadata': {'resultset': {'offset': 1, 'count': 5, 'limit': 25}}, 'results': [{'name': 'Computed', 'id': 'COMP'}, {'name': 'Precipitation', 'id': 'PRCP'}, {'name': 'Air Temperature', 'id': 'TEMP'}, {'name': 'Wind', 'id': 'WIND'}, {'name': 'Weather Type', 'id': 'WXTYPE'}]}
-"""
+
+
+# notes
 # resp = requests.get('https://www.google.com')
 # if resp.status_code != 200:
 #     # This means something went wrong.
 #     print('something went wrong')
 # else:
 #     print(resp)
-
-# https://stackoverflow.com/questions/29927841/noaa-weather-rest-api-causes-error-when-requesting-with-curl
-# curl -H "token:<token>" "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CITY&sortfield=name&sortorder=desc"
