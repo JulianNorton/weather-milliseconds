2b5f99cd738acd46aba0931ecacbce8709363cda
Julian Norton <Julian@JulianNorton.com>
2018-03-13 16:54:23 -0400

    changing port to 5001

--- app/app.py
+++ app/app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 cache.init_app(app)
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    # Bind to PORT if defined, otherwise default to 5001.
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
