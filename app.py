d8c5709d0cbb301ce07496a01a4c68296d43ed37
Sebastián Blanco <sebastianabg@ext.inditex.com>
2019-10-18 13:48:50 +0200

    Implement cache logic

--- app.py
+++ app.py
@@ -5,55 +5,65 @@ from flask import Flask, render_template, request
 from flask_caching import Cache
 from htmlmin.minify import html_minify
 
+cache = Cache(config=
+    {
+        'CACHE_TYPE': 'simple',
+        'CACHE_DEFAULT_TIMEOUT': 1800
+    })
+
 app = Flask(__name__)
 
-cache = Cache(config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
-
 
 @app.route('/')
 @app.route('/index')
-def index():
+@app.route('/<zip_code>')
+def index(zip_code=None):
 
-    timestamp = datetime.datetime.now()
+    if cache.get(zip_code):
+        return render_template('index.html', **cache.get(zip_code))
+    else:
 
-    # // TODO -- MAKE THIS ACCEPT ANY LAT LONG AS A INPUT
-    # latitude and Longitude and  of the area
-    # NYC KNYC central park weather station is Lat: 40.78°N Lon: 73.97°W
-    # long is not negative here because "TOP" in the request accounts for this.
-    zip_code = request.args.get('zip_code')
-    lat, lng, default_value = 0, 0, False
-    with open('./assets/data.csv', 'r') as f:
-        for line in f:
-            data = line.split(',')
-            if data[0] == zip_code:
-                lat, lng = data[1], data[2].rstrip('\n')
-                break
+        timestamp = datetime.datetime.now()
 
-    if lat == 0 and lng == 0:
-        lat, lng, default_value = 40.78, 73.97, True
+        lat, lng, default_value = 0, 0, False
 
-    area_data = requests.get(
-        f'https://api.weather.gov/points/{lat},{lng}'
-    ).json()
-    
-    cwa, gridX, gridY = area_data['properties']['cwa'], area_data['properties']['gridX'], area_data['properties']['gridY']
+        if zip_code is None:
+            zip_code, default = 10019, True #New York, Manhattan, Central Park (default)
 
-    weather_data = requests.get(
-        f'https://api.weather.gov/gridpoints/{cwa}/{gridX},{gridY}/forecast'
-    ).json()
+        with open('./assets/data.csv', 'r') as f:
+            for line in f:
+                data = line.split(',')
+                if data[0] == zip_code:
+                    lat, lng = data[1], data[2].rstrip('\n')
+                    break
 
-    periodic_data = weather_data['properties']['periods']
+        if lat == 0 and lng == 0:
+            lat, lng, default_value = 40.76, -73.98, True
 
-    areaDescription = area_data['properties']['relativeLocation']['properties']['city'] + ', ' + area_data['properties']['relativeLocation']['properties']['state'] +['',' (Default value)'][default_value]
+        area_data = requests.get(
+            f'https://api.weather.gov/points/{lat},{lng}'
+        ).json()
+        
+        cwa, gridX, gridY = area_data['properties']['cwa'], area_data['properties']['gridX'], area_data['properties']['gridY']
 
-    context = {
-        'timestamp': timestamp,
-        'periodic_data': periodic_data,
-        'areaDescription': areaDescription
-    }
+        weather_data = requests.get(
+            f'https://api.weather.gov/gridpoints/{cwa}/{gridX},{gridY}/forecast'
+        ).json()
 
-    return render_template('index.html', **context)
+        periodic_data = weather_data['properties']['periods']
+
+        areaDescription = area_data['properties']['relativeLocation']['properties']['city'] + ', ' + area_data['properties']['relativeLocation']['properties']['state'] +['',' (Default value)'][default_value]
+
+        context = {
+            'timestamp': timestamp,
+            'periodic_data': periodic_data,
+            'areaDescription': areaDescription
+        }
+
+        cache.set(zip_code, context)
+
+        return render_template('index.html', **context)
 
 
 @app.after_request
